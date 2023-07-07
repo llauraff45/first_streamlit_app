7feb980a8063d87c95360345304161600e963d28
llauraff45 <105805440+llauraff45@users.noreply.github.com>
2023-07-07 12:55:15 +0200

    Update streamlit_app.py
    añado fruit_selected como variable q recoge el tema del multibox

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,13 +13,13 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list here so they can pick the fruit they want to include
 #List selected
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+//fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 #we'll ask to use the fruits in our fruits_selected list to pull rows from the full data set (and assign that data to a variable called fruits_to_show)
 fruit_to_show = my_fruit_list.loc([fruits_selected])
 #display the table on the page
